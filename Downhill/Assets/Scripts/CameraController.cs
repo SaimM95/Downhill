9707ff626e4819204bdd1304f679dbe23af73233
Saim Malik <saim95@gmail.com>
2018-02-04 17:38:14 -0500

    can now move the ball around and camera follows the ball

---
+++ Downhill/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start () {
+		// Get the offset between the camera and player object (i.e. a vector going from camera to player)
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		// Move the camera along with the player
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Downhill/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Controls speed of ball movement
+	public float speed;
+
+	// Store reference to 'Rigid Body' component
+	private Rigidbody rigidBody;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		// Apply vertical and horizontal forces to the rigid body
+		Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+		rigidBody.AddForce (movement * speed);
+		
+	}
+}
